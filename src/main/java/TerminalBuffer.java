268d5bd692511630f4da355ea4b914d54f1de721
Sven Farwig <Sven.farwig@rwth-aachen.de>
2026-03-09 16:40:13 +0100

    [Feature] Setup, Attributes, Cursor and Editing

---
+++ src/main/java/TerminalBuffer.java
@@ -0,0 +1,255 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class TerminalBuffer {
+    private int width, height, maxScrollback;
+    private int foregroundColor, backgroundColor;
+    private int cursorPositionColumn, cursorPositionRow;
+    private Cell[][] screen, scrollback;
+    private boolean bold, italic, underline;
+
+    public static void main(String[] args) {
+
+    }
+
+    public TerminalBuffer(int width, int height, int maxScrollback) {
+        screen = new Cell[height][width];
+        scrollback = new Cell[maxScrollback][width];
+    }
+
+    public void clearScreen() {
+        screen = new Cell[height][width];
+    }
+    
+    public void clearScreenAndScrollback() {
+        screen = new Cell[height][width];
+        scrollback = new Cell[maxScrollback][width];
+    }
+
+    public void writeText(String text) {
+        char[] charArray = text.toCharArray();
+        for(char c : charArray) {
+            writeCharacter(cursorPositionRow, cursorPositionColumn, c);
+            if(cursorPositionRow == height - 1 && cursorPositionColumn == width - 1) {
+                insertEmptyLine();
+            }
+            moveCursorRight(1);
+        }
+    }
+
+    public void insertText(String text) {
+        List<Cell> append = new LinkedList<>();
+        if (!lineIsEmpty(cursorPositionRow, cursorPositionColumn)) {
+            for(int j = cursorPositionColumn; j < width; j++) {
+                append.add(screen[cursorPositionRow][j]);
+            }
+        }
+        char[] charArray = text.toCharArray();
+        for(char c : charArray) {
+            writeCharacter(cursorPositionRow, cursorPositionColumn, c);
+            if(cursorPositionRow == height - 1 && cursorPositionColumn == width - 1) {
+                insertEmptyLine();
+            } else if (cursorPositionColumn == width - 1) {
+                insertEmptyLineAt(cursorPositionRow + 1);
+            }
+            moveCursorRight(1);
+        }
+        while(!append.isEmpty()) {
+            if(screen[cursorPositionRow][cursorPositionColumn] != null)
+                append.add(screen[cursorPositionRow][cursorPositionColumn]);
+            screen[cursorPositionRow][cursorPositionColumn] = append.getFirst();
+            append.removeFirst();
+            if(cursorPositionRow == height - 1 && cursorPositionColumn == width - 1) {
+                insertEmptyLine();
+            } else if (cursorPositionColumn == width - 1) {
+                insertEmptyLineAt(cursorPositionRow + 1);
+            }
+            moveCursorRight(1);
+        }
+    }
+
+    private void insertEmptyLineAt(int row) {
+        if (!lineIsEmpty(row, 0)) {
+            int moveDownTo = row - 1;
+            for(int i = row; i < height; i++) {
+                if(lineIsEmpty(i, 0)) {
+                    moveDownTo = i;
+                    break;
+                }
+            }
+            if(moveDownTo < row) {
+                moveCursorUp(1);
+                for(int i = 1; i < maxScrollback; i++) {
+                    System.arraycopy(scrollback[i], 0, scrollback[i - 1], 0, width);
+                }
+                System.arraycopy(scrollback[maxScrollback - 1], 0, screen[0], 0, width);
+                for(int i = 1; i < row; i++) {
+                    System.arraycopy(screen[i], 0, screen[i - 1], 0, width);
+                }
+                for(int j = 0; j < width; j++) {
+                    writeEmptyCell(height - 1, j);
+                }
+            } else {
+                for(int i = moveDownTo; i > row; i--) {
+                    System.arraycopy(screen[i - 1], 0, screen[i], 0, width);
+                }
+            }
+            for(int j = 0; j < width; j++) {
+                writeEmptyCell(row, j);
+            }
+        }
+    }
+
+    public void insertEmptyLine() {
+        moveCursorUp(1);
+        for(int i = 1; i < maxScrollback; i++) {
+            System.arraycopy(scrollback[i], 0, scrollback[i - 1], 0, width);
+        }
+        System.arraycopy(scrollback[maxScrollback - 1], 0, screen[0], 0, width);
+        for(int i = 1; i < height; i++) {
+            System.arraycopy(screen[i], 0, screen[i - 1], 0, width);
+        }
+        for(int j = 0; j < width; j++) {
+            writeEmptyCell(height - 1, j);
+        }
+    }
+
+    public void fillLine() {
+        for(int i = cursorPositionColumn; i < width; i++) {
+            writeEmptyCell(cursorPositionRow, i);
+        }
+    }
+
+    public void fillLine(char character) {
+        for(int i = cursorPositionColumn; i < width; i++) {
+            writeCharacter(cursorPositionRow, i, character);
+        }
+    }
+
+    private boolean lineIsEmpty(int row, int start) {
+        if(row >= 0 && row < height && start < width && start >= 0) {
+            for(int j = start; j < width; j++) {
+                if(screen[row][j] != null)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private void writeEmptyCell(int row, int column) {
+        screen[row][column] = null;
+    }
+
+    private void writeCharacter(int row, int column, char character) {
+        screen[row][column] = new Cell(foregroundColor, backgroundColor, character, bold, italic, underline);
+    }
+
+    public void moveCursorUp(int NCells) {
+        if(NCells > 0) {
+            if (NCells <= cursorPositionRow)
+                cursorPositionRow -= NCells;
+            else
+                cursorPositionRow = 0;
+        }
+    }
+
+    public void moveCursorDown(int NCells) {
+        if(NCells > 0) {
+            if (NCells + cursorPositionRow < height)
+                cursorPositionRow += NCells;
+            else
+                cursorPositionRow = height - 1;
+        }
+    }
+
+    public void moveCursorRight(int NCells) {
+        if(NCells > 0) {
+            if (NCells + cursorPositionColumn < width)
+                cursorPositionColumn += NCells;
+            else if (cursorPositionRow + 1 < height) {
+                moveCursorDown(1);
+                NCells -= (width - cursorPositionColumn);
+                cursorPositionColumn = 0;
+                moveCursorRight(NCells);
+            } else
+                //Cursor is in the bottom right
+                cursorPositionColumn = width - 1;
+        }
+    }
+
+    public void moveCursorLeft(int NCells) {
+        if(NCells > 0) {
+            if (NCells <= cursorPositionColumn)
+                cursorPositionColumn -= NCells;
+            else if (cursorPositionRow > 0) {
+                moveCursorUp(1);
+                NCells -= cursorPositionColumn;
+                cursorPositionColumn = width - 1;
+                moveCursorLeft(NCells);
+            } else
+                //Cursor is in the top left
+                cursorPositionColumn = 0;
+        }
+    }
+
+    public void setForegroundColor(int foregroundColor) {
+        this.foregroundColor = foregroundColor;
+    }
+
+    public void setBackgroundColor(int backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public void setBold(boolean bold) {
+        this.bold = bold;
+    }
+
+    public void setItalic(boolean italic) {
+        this.italic = italic;
+    }
+
+    public void setUnderline(boolean underline) {
+        this.underline = underline;
+    }
+
+    public void setCursorPositionColumn(int cursorPositionColumn) {
+        if(cursorPositionColumn < 0)
+            this.cursorPositionColumn = 0;
+        else if(cursorPositionColumn >= width)
+            this.cursorPositionColumn = width - 1;
+        else
+            this.cursorPositionColumn = cursorPositionColumn;
+    }
+
+    public void setCursorPositionRow(int cursorPositionRow) {
+        if(cursorPositionRow < 0)
+            this.cursorPositionRow = 0;
+        else if(cursorPositionRow >= height)
+            this.cursorPositionRow = height - 1;
+        else
+            this.cursorPositionRow = cursorPositionRow;
+    }
+
+    public int getCursorPositionColumn() {
+        return cursorPositionColumn;
+    }
+
+    public int getCursorPositionRow() {
+        return cursorPositionRow;
+    }
+
+    private class Cell {
+        private int foregroundColor, backgroundColor;
+        private char character;
+        private boolean bold, italic, underline;
+
+        private Cell(int foregroundColor, int backgroundColor, char character, boolean bold, boolean italic, boolean underline) {
+            this.foregroundColor = foregroundColor;
+            this.backgroundColor = backgroundColor;
+            this.character = character;
+            this.bold = bold;
+            this.italic = italic;
+            this.underline = underline;
+        }
+    }
+}
